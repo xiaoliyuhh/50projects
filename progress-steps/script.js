b871ac23f6601b68a394763672ba224924c0d307
xly <2745304733@qq.com>
2023-09-06 23:41:55 +0800

    primal

---
+++ progress-steps/script.js
@@ -0,0 +1,12 @@
+const circles = document.querySelectorAll('.circle')
+const pre = document.getElementById('pre')
+const next = document.getElementById('next')
+
+circles.forEach(circle => {
+
+})
+function removeActive() {
+    circles.forEach(circle => {
+        circle.classList.remove('active')
+    })
+}
