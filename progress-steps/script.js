ed1cdd9c395eb2cb561a1cc00eed3203198f9f24
xly <2745304733@qq.com>
2023-09-07 21:45:49 +0800

    new version

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,12 +1,45 @@
 const circles = document.querySelectorAll('.circle')
 const pre = document.getElementById('pre')
 const next = document.getElementById('next')
+const progress = document.getElementById('progress')
 
-circles.forEach(circle => {
-
+let currentDone = 1;
+next.addEventListener('click', () => {
+    currentDone++;
+    if (currentDone > circles.length) {
+        currentDone = circles.length;
+    }
+    update();
 })
-function removeActive() {
-    circles.forEach(circle => {
-        circle.classList.remove('active')
+pre.addEventListener('click', () => {
+    currentDone--;
+    if (currentDone < 1) {
+        currentDone = 1;
+    }
+    update();
+})
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < currentDone) {
+            circle.classList.add('active');
+        }
+        else {
+            circle.classList.remove('active');
+        }
     })
+    const actives = document.querySelectorAll('.active.circle');
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if (currentDone === 1) {
+        pre.disabled = true;
+    }
+    else if (currentDone === circles.length) {
+        next.disabled = true
+    }
+    else {
+        pre.disabled = false;
+        next.disabled = false;
+    }
 }
+
